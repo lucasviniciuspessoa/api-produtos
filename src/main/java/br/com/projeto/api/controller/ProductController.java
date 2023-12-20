a5045316e01cff220ed9fcd5336eecf0ef4e7226
Lucas Vinicius <lucaviniciuspessoaf@gmail.com>
2023-12-19 23:02:40 -0300

    feat: add CORS

--- src/main/java/br/com/projeto/api/controller/ProductController.java
+++ src/main/java/br/com/projeto/api/controller/ProductController.java
@@ -2,6 +2,7 @@ package br.com.projeto.api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +15,7 @@ import br.com.projeto.api.model.ProductModel;
 import br.com.projeto.api.services.ProductService;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class ProductController {
 
     @Autowired
