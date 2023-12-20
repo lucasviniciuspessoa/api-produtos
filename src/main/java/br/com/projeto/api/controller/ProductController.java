5008f3183f8e4da1ce599c1ec205035d3372c356
Lucas Vinicius <lucaviniciuspessoaf@gmail.com>
2023-12-19 22:56:06 -0300

    feat: add register and alter route

--- src/main/java/br/com/projeto/api/controller/ProductController.java
+++ src/main/java/br/com/projeto/api/controller/ProductController.java
@@ -1,7 +1,11 @@
 package br.com.projeto.api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.projeto.api.model.ProductModel;
@@ -17,5 +21,14 @@ public class ProductController {
     @GetMapping("/")
     public Iterable<ProductModel> list() {
         return ps.list();
+    }
+    @PostMapping("/register")
+    public ResponseEntity<?> register(@RequestBody ProductModel pm) {
+        return ps.registerAlter(pm, "register");
+    }
+
+    @PutMapping("/alter")
+    public ResponseEntity<?> alter(@RequestBody ProductModel pm) {
+        return ps.registerAlter(pm, "alterar");
     }
 }
--- src/main/java/br/com/projeto/api/services/ProductService.java
+++ src/main/java/br/com/projeto/api/services/ProductService.java
@@ -1,9 +1,12 @@
 package br.com.projeto.api.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import br.com.projeto.api.model.ProductModel;
+import br.com.projeto.api.model.ResponseModel;
 import br.com.projeto.api.repository.ProductRepository;
 
 @Service
@@ -13,9 +16,31 @@ public class ProductService {
     @Autowired
     private ProductRepository pr;
 
+    @Autowired
+    private ResponseModel rm;
 
     public Iterable<ProductModel> list() {
         return pr.findAll();
     }
 
+    public ResponseEntity<?> registerAlter(ProductModel pm, String action) {
+        if(pm.getName().equals("")) {
+            rm.setMessage("O nome do produto é obrigatório.");  
+            return new ResponseEntity<ResponseModel>(rm, HttpStatus.BAD_REQUEST);
+        } else if(pm.getBrand().equals("")) {
+            rm.setMessage("O nome da marca é obrigatório.");
+            return new ResponseEntity<ResponseModel>(rm, HttpStatus.BAD_REQUEST);
+        } else {
+            if(action.equals("register")) {
+                 return new ResponseEntity<ProductModel>(pr.save(pm), HttpStatus.CREATED);
+            } else {
+                 return new ResponseEntity<ProductModel>(pr.save(pm), HttpStatus.OK);
+
+            }
+
+
+        }
+        
+    }
+ 
 }
